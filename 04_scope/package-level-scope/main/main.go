440de39461081b587496c88bce748e11f449ca03
Sectan <dr.6010@gmail.com>
2017-07-13 20:26:08 +0200

    Update 04_scope/package-level-scope

--- 04_scope/package-level-scope/main/main.go
+++ 04_scope/package-level-scope/main/main.go
@@ -5,7 +5,13 @@ import (
 	"github.com/sectan/GolangFundamentals/04_scope/package-level-scope/valpkg"
 )
 
+var foo = 1
+
 func main() {
 	fmt.Println(valpkg.FirstName)
 	valpkg.PrintValues()
+	fmt.Println(foo)
+	fmt.Println(bar)
 }
+
+var bar = 1
