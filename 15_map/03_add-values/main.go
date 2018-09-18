d954d99c4978cab65e272a3430c23cf13459ef17
Sectan <dr.6010@gmail.com>
2018-09-18 22:29:55 +0200

    Improve 15_map/03_add-values

--- 15_map/03_add-values/main.go
+++ 15_map/03_add-values/main.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func main() {
 	players := make(map[int]string)
-	players[10] = "Totti"
+	players[10] = "?"
 	players[16] = "de Rossi"
 	players[24] = "Florenzi"
+	fmt.Println(players)
 
+	players[10] = "Totti" // Replace the old value
 	fmt.Println(players)
 }
